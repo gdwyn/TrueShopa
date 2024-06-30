0e8d74ce9a9c1001b3c06b79819517ef3a04351e
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-06-30 19:14:19 +0100

    Initial Commit

---
+++ TrueShopa/TrueShopaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueShopaApp.swift
+//  TrueShopa
+//
+//  Created by Godwin IE on 30/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrueShopaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
